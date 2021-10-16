017cea50d8c068facd6caed332d244af46fa33d2
Alina <tahera.firdose@gmail.com>
2021-10-16 10:07:55 +0530

    assignment2

---
+++ public/Scripts/app.js
@@ -0,0 +1,31 @@
+const e = require("express");
+
+// IIFE -- Immediately Invoked Function Expression
+(function () {
+    function Start()
+    {
+        console.log("App Started...");
+
+        let deleteButtons = document.querySelectorAll('.btn-danger');
+        
+        for(button of deleteButtons)
+        {
+            button.addEventListener('click', (event)=>{
+                if(!confirm("Are you sure?")) 
+                {
+                    event.preventDefault();
+                    window.location.assign('/business-contact-list');
+                }
+            });
+        }
+    }
+
+    window.addEventListener("load", Start);
+
+})();
+
+// alert message on submission
+function clicked() {
+    return confirm("Successfully Submitted");
+}
+
---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+  //  "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://Tahera:rOabUFNMh1aHAeNf@cluster0.c7vqo.mongodb.net/business-contact?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
